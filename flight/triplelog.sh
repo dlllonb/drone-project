704905d163ea1b1e0e082287e76a48093cd24f39
dlllonb <bassdillon10@gmail.com>
2025-10-01 11:15:42 -0400

    added bash to run 3 loggers

---
+++ flight/triplelog.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+set -m
+
+python3 GPS/GPS.py &
+GPS_PID=$!
+python3 IMU/IMU.py &
+IMU_PID=$!
+python3 CAM?CAM.py &
+CAM_PID=$!
+
+trap 'echo; echo "Stopping…"; kill -INT -$PGID' INT TERM
+PGID=$(ps -o pgid= $$ | tr -d ' ')
+wait
