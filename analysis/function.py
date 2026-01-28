590e7d97fe1f6ed944ab9d7062a151f891410754
dlllonb <bassdillon10@gmail.com>
2026-01-28 15:17:11 -0500

    first set of code to get predicted known angles

--- analysis/function.py
+++ analysis/function.py
@@ -1,5 +1,19 @@
 # file to contain functions called elsewhere for the pipeline
+import function
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def imports():
+    '''Just make sure we can import the functions in this file'''
     print("Successfully imported functions.")
     return
+
+def unit(v: np.ndarray) -> np.ndarray:
+    '''Normalize to a unit vector'''
+    v = np.asarray(v, dtype=float).reshape(3)
+    n = np.linalg.norm(v)
+    if n == 0:
+        raise ValueError("Zero-length vector cannot be normalized.")
+    return v / n
