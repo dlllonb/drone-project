13f4582653e64d94fb56b6b035ca5e417be6ea08
Winston Zhang <winston.y.zhang@gmail.com>
2026-05-01 17:48:35 -0400

    Add encoder value sanity test

--- ground/test/run_unit_tests.sh
+++ ground/test/run_unit_tests.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 python3 -m pytest test/unit/test_load_config.py
 python3 -m pytest test/unit/test_create_plot.py
 python3 -m pytest test/unit/test_create_animation.py
+python3 -m pytest test/unit/test_encoder_pkl_sanity.py
 
 bash test/unit/test_process_exposures_batch.sh
